31137c75348148cac58f02677fae7743c32b5b91
s1088475 <s1088475@student.knoxschools.org>
2026-09-24 01:13:17 +0000

    Completed food dict and got outlines from teacher

--- main.py
+++ main.py
@@ -1,5 +1,54 @@
 foods = {
-    'cheeseburger': {'price': 10.00, 'in_stock': True},
-    'grilled_chicken_sandwhich': {'price': 12.00, 'in_stock': True},
-    'fries': {'price': 4.00, 'in_stock': True}
+    # Specialties
+    'chicken finger platter': {'price': 16.00, 'in_stock': True},
+    'buttermilk fried chicken': {'price': 18.00, 'in_stock': True},
+    'fish and chips': {'price': 18.00, 'in_stock': True},
+    'north carolina catfish': {'price': 16.00, 'in_stock': True},
+    'boston scrod': {'price': 18.00, 'in_stock': True},
+
+    # Favorites
+    'citrus glazed salmon': {'price': 26.00, 'in_stock': True},
+    'lemon lime chicken': {'price': 16.00, 'in_stock': True},
+    'hickory chicken': {'price': 18.00, 'in_stock': True},
+    'steak on a stick': {'price': 24.00, 'in_stock': True},
+    'center cut sirloin': {'price': 24.00, 'in_stock': True},
+    'ribeye 12oz': {'price': 30.00, 'in_stock': True},
+    'ribeye 16oz': {'price': 40.00, 'in_stock': True},
+    'hawaiian ribeye': {'price': 39.00, 'in_stock': True},
+    'filet mignon 6oz': {'price': 34.00, 'in_stock': True},
+    'filet mignon 9oz': {'price': 42.00, 'in_stock': True},
+
+    # Pasta
+    'haystack pasta': {'price': 18.00, 'in_stock': True},
+    'tomato basil': {'price': 17.00, 'in_stock': True},
+    'rattlesnake pasta': {'price': 18.00, 'in_stock': True},
+    'seafood pasta': {'price': 17.00, 'in_stock': True},
+
+    # Drinks - wine
+    'banfi maschio prosecco': {'price': 7.00, 'in_stock': True},
+    'klinker brick bricks roses rose': {'price': 9.00, 'in_stock': True},
+    'schmitt sohne relax riesling': {'price': 8.00, 'in_stock': True},
+    'benvolio pinot grigio': {'price': 8.00, 'in_stock': True},
+    'kim crawford sauvignon blanc': {'price': 10.00, 'in_stock': True},
+    'rodney strong chardonnay': {'price': 7.00, 'in_stock': True},
+    'mer soleil silver chardonnay': {'price': 10.00, 'in_stock': True},
+    'wente bailey hill pinot noir': {'price': 8.00, 'in_stock': True},
+    'rodney strong russian river pinot noir': {'price': 10.00, 'in_stock': True},
+    'j lohr merlot': {'price': 10.00, 'in_stock': True},
+    'gundlach bundschu mountain cuvee': {'price': 11.00, 'in_stock': True},
+    'elou red blend': {'price': 9.00, 'in_stock': True},
+    'conundrum red blend': {'price': 10.00, 'in_stock': True},
+    'rodney strong cabernet sauvignon': {'price': 12.00, 'in_stock': True},
+    'serial cabernet sauvignon': {'price': 10.00, 'in_stock': True}
 }
+
+# orders = []
+
+# orders.append(foods['conundrum red blend'])
+# orders.append(foods['mer soleil silver chardonnay'])
+
+# print(orders)
+
+# bill = {'paid': False, 'food_ordered': orders}
+
+# print(bill)
